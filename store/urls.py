f876c15abf71cff1eae6bee19dfc2371cc95d43c
LIA HOS <sohailpatel71@yahoo.com>
2021-05-20 20:22:39 +0530

    ecommerce

---
+++ store/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+	path('register/', views.registerPage, name="register"),
+	path('login/', views.loginPage, name="login"),  
+	path('logout/', views.logoutUser, name="logout"),
+	#Leave as empty string for base url
+	path('', views.store, name="store"),
+	path('cart/', views.cart, name="cart"),
+	path('checkout/', views.checkout, name="checkout"),
+	path('update_item/', views.updateItem, name="update_Item"),
+	path('process_order/', views.processOrder, name="process_order"),
+	
+
+]
